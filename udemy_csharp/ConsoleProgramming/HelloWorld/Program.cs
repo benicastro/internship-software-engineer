a615b785f12ab01aa9d121011f71865897aa6459
benicastro <benedict.zcastro@gmail.com>
2023-10-04 02:20:18 +0800

    Wrote a code about input/output functionalities in C#

--- udemy_csharp/ConsoleProgramming/HelloWorld/Program.cs
+++ udemy_csharp/ConsoleProgramming/HelloWorld/Program.cs
@@ -1,3 +1,24 @@
 ﻿//See https://aka.ms/new-console-template for more information
+
+// This line prints "Hello, World!"
 Console.WriteLine("Hello, World!");
 
+/* 
+
+There are three control structures in C# Programming:
+    - Sequence
+    - Decision
+    - Repetion
+*/
+
+var name = "Beni";
+Console.WriteLine(name);
+
+if (name == "Beni")
+{
+    Console.WriteLine("Okay");
+}
+else
+{
+    Console.WriteLine("Oh no!");
+}
---
+++ udemy_csharp/ConsoleProgramming/InputOutput/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Declare Variable
+/*
+ * Data Types:
+ *      char
+ *      string
+ *      int
+ *      long
+ *      double
+ *      float
+ *      bool
+*/
+string fullName;
+
+// Allow user input and store in a variable
+Console.WriteLine("Please provide your full name: ");
+fullName = Console.ReadLine();
+
+// Print contents of variable containing the user's input
+Console.WriteLine($"Your name is {fullName}.");
+
