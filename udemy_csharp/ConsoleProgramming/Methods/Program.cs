c2ee3ece99c8d88b3af2f4324e2c95d369d3bc87
benicastro <benedict.zcastro@gmail.com>
2023-10-05 02:23:37 +0800

    Worked on methods that return values

--- udemy_csharp/ConsoleProgramming/Methods/Program.cs
+++ udemy_csharp/ConsoleProgramming/Methods/Program.cs
@@ -10,6 +10,7 @@
 // YAGNI - You Aren't Going to Need It
 
 // Void Functions
+
 Console.WriteLine();
 
 void PrintName(string name)
@@ -24,5 +25,35 @@ void Addition(int num1, int num2)
 
 PrintName("Beni");
 Addition(5, 7);
+Console.WriteLine();
 
 // Value Returning Functions
+
+Console.Write("Enter first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int LargestNumber(int num1, int num2, int num3)
+{
+    int largest = num1;
+
+    if (largest < num2)
+    {
+        largest = num2;
+    }
+
+    if (largest < num3)
+    {
+        largest = num3;
+    }
+
+    return largest;
+}
+
+int result = LargestNumber(num1, num2, num3);
+Console.WriteLine($"The largest number is {result}.");
