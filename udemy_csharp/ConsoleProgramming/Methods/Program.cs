639573a8241648472c4f0b83afaa4906fa7eb12e
benicastro <benedict.zcastro@gmail.com>
2023-10-05 01:40:29 +0800

    Created a project to demonstrate how methods work in C#

---
+++ udemy_csharp/ConsoleProgramming/Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * Prototype - defines the function (type, name, parameters)
+ * Definition - has the code, contains the code block
+ * Function Call - executes the function
+*/
+
+// DRY - Don't Repeat Yourself
+// YAGNI - You Aren't Going to Need It
+
+// Void Functions
+Console.WriteLine();
+
+void PrintName(string name)
+{
+    Console.WriteLine(name);
+}
+
+void Addition(int num1, int num2)
+{
+    Console.WriteLine($"The sum of {num1} and {num2} is {num1 + num2}.");
+}
+
+PrintName("Beni");
+Addition(5, 7);
+
+// Value Returning Functions
