7f6b166940ead040ea2fb2ce0019142d66b0e2e9
benicastro <benedict.zcastro@gmail.com>
2023-10-04 20:39:28 +0800

    Created a project exploring operators in C#

---
+++ udemy_csharp/ConsoleProgramming/MathOperators/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Basic Assignment Operators
+int num1 = 11;
+int num2 = 12;
+
+// Arithmetic Operators
+Console.WriteLine($"Addition: {num1 + num2}");
+Console.WriteLine($"Subtraction: {num1 - num2}");
+Console.WriteLine($"Multiplication: {num1 * num2}");
+Console.WriteLine($"Division: {num1 / num2}");
+Console.WriteLine($"Modulus: {num1 % num2}");
+
+// Compount Assignment Operators
+num1 += 4;
+Console.WriteLine(num1);
+num1 -= 4;
+Console.WriteLine(num1);
+num1 *= 4;
+Console.WriteLine(num1);
+num1 /= 4;
+Console.WriteLine(num1);
+num1 %= 4;
+Console.WriteLine(num1);
+
