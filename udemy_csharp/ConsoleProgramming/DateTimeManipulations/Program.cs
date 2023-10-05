de106137d9aa1c8141c583f65fed5ced92a51f27
benicastro <benedict.zcastro@gmail.com>
2023-10-06 00:58:22 +0800

    Started working on DateTime in C#

--- udemy_csharp/ConsoleProgramming/DateTimeManipulations/Program.cs
+++ udemy_csharp/ConsoleProgramming/DateTimeManipulations/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// Create a DateTime from date and time
+
+// Create a DateTime from current timestamp
+
+// Create a DateTime from a String
+
+// Empty DateTime
+
+// DateTime from Ticks
+
+// Date Only
+
+// Time Only
