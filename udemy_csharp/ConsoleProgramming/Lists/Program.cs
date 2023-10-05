40d9c05f7ca01148ed96e6fc12a16536036789c7
benicastro <benedict.zcastro@gmail.com>
2023-10-06 03:31:47 +0800

    Created a project tackling Lists in C#

---
+++ udemy_csharp/ConsoleProgramming/Lists/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Declare
+List<string> names = new List<string>();
+string name = string.Empty;
+
+// Add values to list
+names.Add("Castro");
+//names.Remove("Beni");
+
+
+while (!name.Equals("-1"))
+{
+    Console.Write("Enter Name: ");
+    name = Console.ReadLine();
+    if (!string.IsNullOrEmpty(name) && !name.Equals("-1"))
+    {
+        names.Add(name);
+        Console.WriteLine($"{name} was added successfully");
+    }   
+}
+
+// Print values in list
+foreach (string addedName in names)
+{
+    Console.WriteLine(addedName);
+}
+
+for (int i = 0; i < names.Count; i++)
+{
+    Console.WriteLine(names[i]);
+}
