9f9c4717bc74cdaf92d4e8d7678cbccec9efd5f6
benicastro <benedict.zcastro@gmail.com>
2023-10-11 03:21:41 +0800

    Worked on Object-Oriented Programming in C#

---
+++ udemy_csharp/ConsoleProgramming/ClassesAndObjects/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    public class Person
+    {
+        // Properties when public
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        // Fields
+        private double _salary { get; set; }
+
+        // Define a method
+        public void setSalary(double salary)
+        {
+            _salary = salary;
+        }
+
+        public double getSalary() 
+        { 
+            return _salary;
+        }
+
+        public string getFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
---
+++ udemy_csharp/ConsoleProgramming/ClassesAndObjects/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Primitive Data Types - int, string, bool, char
+
+// Object-Oriented Programming - creating your own data type
+// Class - blueprint
+
+// Single Responsibility Principle
+
+using ClassesAndObjects;
+
+Person person =  new();
+
+Console.Write("Enter First Name: ");
+person.FirstName = Console.ReadLine();
+
+Console.Write("Enter Last Name: ");
+person.LastName = Console.ReadLine();
+
+Console.Write("Enter Age: ");
+person.Age = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter salary: ");
+int salary = Convert.ToInt32(Console.ReadLine());
+
+person.setSalary(salary);
+
+Console.WriteLine($"Full name is: {person.getFullName()}");
+Console.WriteLine($"Age is: {person.Age}");
+Console.WriteLine($"Salary is: {person.getSalary()}");
