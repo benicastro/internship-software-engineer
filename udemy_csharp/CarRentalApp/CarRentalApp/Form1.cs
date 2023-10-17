2597a6c9b499941cad7114d63856edd7e3f0e460
benicastro <benedict.zcastro@gmail.com>
2023-10-18 01:48:23 +0800

    Continued working on the Car Rental Application Winform using C#

--- udemy_csharp/CarRentalApp/CarRentalApp/Form1.cs
+++ udemy_csharp/CarRentalApp/CarRentalApp/Form1.cs
@@ -16,5 +16,45 @@ namespace CarRentalApp
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
