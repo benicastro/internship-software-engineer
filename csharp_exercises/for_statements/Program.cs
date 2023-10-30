606ab4877704ba9a95b77072a319d2983ae31d14
benicastro <benedict.zcastro@gmail.com>
2023-10-31 04:14:44 +0800

    Worked on for and while statements in C#

---
+++ csharp_exercises/for_statements/Program.cs
@@ -0,0 +1,42 @@
+﻿// for (int i = 0; i < 10; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+// string[] names = { "Alex", "Eddie", "David", "Michael" };
+// for (int i = 0; i < names.Length; i++)
+//     if (names[i] == "David") names[i] = "Sammy";
+
+// foreach (var name in names) Console.WriteLine(name);
+
+// for (int i = 1; i < 101; i++)
+// {
+//     if (i % 5 == 0 & i % 3 == 0)
+//     {
+//         Console.WriteLine($"{i} - FizzBuzz");
+//     }
+//     else if (i % 5 == 0)
+//     {
+//         Console.WriteLine($"{i} - Buzz");
+//     }
+//     else if (i % 3 == 0)
+//     {
+//         Console.WriteLine($"{i} - Fizz");
+//     }
+//     else
+//     {
+//         Console.WriteLine(i);
+//     }
+// }
+
+for (int i = 1; i < 101; i++)
+{
+    if ((i % 3 == 0) && (i % 5 == 0))
+        Console.WriteLine($"{i} - FizzBuzz");
+    else if (i % 3 == 0)
+        Console.WriteLine($"{i} - Fizz");
+    else if (i % 5 == 0)
+        Console.WriteLine($"{i} - Buzz");
+    else
+        Console.WriteLine($"{i}");
+}
