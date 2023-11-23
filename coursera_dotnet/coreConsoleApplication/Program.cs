fc3b295bbfdf65b88d6f7358df0aa596605621ab
benicastro <benedict.zcastro@gmail.com>
2023-11-24 00:59:00 +0800

    Added a StringIndexerType class to implement indexer in C#

--- coursera_dotnet/coreConsoleApplication/Program.cs
+++ coursera_dotnet/coreConsoleApplication/Program.cs
@@ -46,10 +46,23 @@ using coreConsoleApplication;
 
 // Properties
 
-User user = new User();
-user.Name = "Test";
-user.Age = 30;
+//User user = new User();
+//user.Name = "Test";
+//user.Age = 30;
 
-Console.WriteLine(user.Name);
-Console.WriteLine(user.Age);
-Console.WriteLine(user.CompanyName);
+//Console.WriteLine(user.Name);
+//Console.WriteLine(user.Age);
+//Console.WriteLine(user.CompanyName);
+
+// Indexer
+
+StringIndexerType stringIndexerType = new StringIndexerType();
+stringIndexerType[0] = "String One";
+stringIndexerType[1] = "String Two";
+stringIndexerType[2] = "String Three";
+stringIndexerType[3] = "String Four";
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(stringIndexerType[i]);
+}
